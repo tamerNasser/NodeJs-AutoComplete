728db62ea147c4979e6de38b6272c3b262f12725
Tamer Nasser <tamernasser@outlook.com>
2019-03-27 10:56:13 +0200

    Fixed fetch bug

--- frontend/scripts/xhr.js
+++ frontend/scripts/xhr.js
@@ -1,25 +1,28 @@
+var countries = [];
 
-let connectToBase = function( value ) {
-// , { "mode" : "no-cors"}
-fetch('/search?input=' + value )
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
+let connectToBase = function(value) {
+  // , { "mode" : "no-cors"}
+  // console.log(value);
+  fetch('/search?input=' + value)
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+
+        response.json().then(function(data) {
+          // console.log(data);
+          countries = data;
+          // console.log(countries);
+        });
       }
-
-      response.json().then(function(data) {
-        console.log(data);
-        countries = JSON.parse(data);
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
+    )
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
 }
 if (typeof module !== "undefined") {
-module.exports = connectToBase;
+  module.exports = connectToBase;
 }
