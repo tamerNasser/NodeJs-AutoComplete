f13afb7985a96317c57176885cc090626edfe377
Tamer Nasser <tamernasser@outlook.com>
2019-03-27 17:45:37 +0200

    Fixed server port

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const router = require("./router");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(router);
 server.listen(port, () => {
   console.log(`server is listening to ${port}`);
