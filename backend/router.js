65fbc160dae26bd073c4075630217adb7f76b9ce
Tamer Nasser <tamernasser@outlook.com>
2019-03-27 20:24:22 +0200

    Organized the code

--- backend/router.js
+++ backend/router.js
@@ -1,17 +1,18 @@
 const handlers = require("./handlers");
 const obj = require('./words.json');
 const data = JSON.stringify(obj);
+
 const router = (request, response) => {
   const url = request.url;
-  if(url === '/'){
+  if (url === '/') {
     handlers.handleHome(response);
-  }else if (url.substring(0,7) === "/search") {
-    handlers.handleAutoComplete(response,url,data);
-  }else if(url.indexOf('.')>0){
-    handlers.handlePublic(response,url);
-  }else if(url.indexOf('?submitvalue')>0){
+  } else if (url.substring(0, 7) === "/search") {
+    handlers.handleAutoComplete(response, url, data);
+  } else if (url.indexOf('.') > 0) {
+    handlers.handlePublic(response, url);
+  } else if (url.indexOf('?submitvalue') > 0) {
     handlers.handleHistory(response, url, data)
-  }else {
+  } else {
     handlers.handleHome(response);
   }
 };
