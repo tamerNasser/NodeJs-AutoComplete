ba6948f18e46ee966043cffa1d6aa88db9ad47d1
Tamer Nasser <tamernasser@outlook.com>
2019-03-27 22:17:16 +0200

    small code refactor

--- frontend/scripts/xhr.js
+++ frontend/scripts/xhr.js
@@ -1,42 +1,37 @@
-var countries = [];
+let words = [];
 
-let connectToBase = function(value,cb) {
-  // , { "mode" : "no-cors"}
-  // console.log(value);
-
-  let lastWord = value.split( ' ' );
+let connectToBase = function(value, cb) {
+  let lastWord = value.split(' ');
   let lastWordF = "";
- if( lastWord[lastWord.length - 1].trim() === "")
- lastWordF = lastWord[0];
 
- else lastWordF = lastWord[lastWord.length - 1]
+  if (lastWord[lastWord.length - 1].trim() === "")
+    lastWordF = lastWord[0];
+  else
+    lastWordF = lastWord[lastWord.length - 1]
 
   fetch('/search?input=' + lastWordF)
     .then(
-      function(response) {
+      (response) => {
         if (response.status !== 200) {
           console.log('Looks like there was a problem. Status Code: ' +
             response.status);
           return;
         }
 
-        response.json().then(function(data) {
-          // console.log(data);
-          countries = data;
-            cb();
-          // console.log(countries);
+        response.json().then((data) => {
+          words = data;
+          cb();
         })
       }
-
     )
-    .catch(function(err) {
+    .catch((err) => {
       console.log('Fetch Error :-S', err);
     });
 
 }
 
-const changeValue = () => {
-  return countries;
+const getAutoCompleteWords = () => {
+  return words;
 }
 
 if (typeof module !== "undefined") {
