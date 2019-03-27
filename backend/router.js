a87a8df06b1e06d212bde9d2196e517e114b670b
Tamer Nasser <tamernasser@outlook.com>
2019-03-27 20:45:56 +0200

    Added timing for data progress

--- backend/router.js
+++ backend/router.js
@@ -1,13 +1,17 @@
 const handlers = require("./handlers");
 const obj = require('./words.json');
+console.time("Stored data in ")
 const data = JSON.stringify(obj);
+console.timeEnd("Stored data in ")
 
 const router = (request, response) => {
   const url = request.url;
   if (url === '/') {
     handlers.handleHome(response);
   } else if (url.substring(0, 7) === "/search") {
+    console.time('Finding the autocomple words took ')
     handlers.handleAutoComplete(response, url, data);
+    console.timeEnd('Finding the autocomple words took ');
   } else if (url.indexOf('.') > 0) {
     handlers.handlePublic(response, url);
   } else if (url.indexOf('?submitvalue') > 0) {
